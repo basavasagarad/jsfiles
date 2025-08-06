4b7316611ecb10081e9702b4258d72a1a6c63075
basavasagarad <156989346+basavasagarad@users.noreply.github.com>
2025-08-06 07:16:19 +0000

    new project

---
+++ projects/unlimitedcolors/colours.js
@@ -0,0 +1,23 @@
+const randomcolor=function(){
+    const hex=" '0123456789ABCDEF";
+    let color='#';
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+let intervalid;
+const startchangingcolor=function(){
+    if(!intervalid){
+        intervalid=setInterval(changebg,1000);
+    }
+        function changebg(){
+            document.body.style.backgroundColor=randomcolor();
+        }
+};
+const stopchangebg=function(){
+    clearInterval(intervalid);
+    intervalid=null;
+};
+document.querySelector('#start').addEventListener('click',startchangingcolor);
+document.querySelector('#stop').addEventListener('click',stopchangebg);
